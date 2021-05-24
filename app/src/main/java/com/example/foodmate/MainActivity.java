4a5ad04ee57cf17e44a7114be52ea2c153ada351
ashpurple <ashpurple9@gmail.com>
2021-05-24 12:41:35 +0900

    Update: token refresh

--- app/src/main/java/com/example/foodmate/MainActivity.java
+++ app/src/main/java/com/example/foodmate/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.foodmate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -13,10 +14,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,12 +34,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Main", "create main");
+        FirebaseAuth mAuth;
+
+        mAuth = FirebaseAuth.getInstance();
 
         // if the user is not logged in
-        if(FirebaseAuth.getInstance().getCurrentUser()==null){
+        if(mAuth.getCurrentUser()==null){
             startMyActivity(LoginActivity.class);
         }
+        // Refresh User token to fireStore
+        String uid=mAuth.getUid();
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("Token", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+                        // Get new FCM registration token
+                        String token = task.getResult();
 
+                        // store to FireStore
+                        Map<String, Object> users = new HashMap<>();
+                        users.put("uid", uid);
+                        users.put("token", token);
+
+                        FirebaseFirestore db;
+                        db = FirebaseFirestore.getInstance();
+                        db.collection("Users").document(uid)
+                                .update(users)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d("Token Save", "DocumentSnapshot successfully written!");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w("Token Save", "Error writing document", e);
+                                    }
+                                });
+                    }
+                });
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
